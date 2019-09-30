41b7ed4bc72e92448d75cd26d4153502af08dd34
Donovan.Xu <d.east6@gmail.com>
2019-09-30 10:59:19 +0800

    react => demo1

---
+++ React/demo1/helloworld_react.js
@@ -0,0 +1,7 @@
+ReactDOM.render(
+    <div>
+      <h1>Hello jsx</h1>
+    </div>
+    ,
+    document.getElementById('app')
+);
---
+++ React/my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Reactjs</h2>
+        
+        <p className="App-intro">
+          你可以在 <code>src/App.js</code> 文件中修改。
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
